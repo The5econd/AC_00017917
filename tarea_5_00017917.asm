169e61c922a0e70407fd47e9637c096054b8b523
The5econd <00017917@uca.edu.sv>
2020-06-23 20:50:48 -0600

    lo hice dinamico para que no quede tan largo el codigo

--- tarea_5_00017917.asm
+++ tarea_5_00017917.asm
@@ -10,38 +10,46 @@ section .text
 	;VERTICALES
 	mov 	si, 100d ; X -> Columna
 	mov 	di, 50d ; Y -> Fila
-	call 	linea1v
+	mov	bl, 150d
+	call 	linea_v
 	mov	si, 120d ; X -> Columna
 	mov 	di, 110d ; Y -> Fila
-	call 	linea3v
+	mov	bl, 150d
+	call 	linea_v
 	mov	si, 150d ; X -> Columna
 	mov 	di, 90d; Y -> Fila
-	call 	linea5v
+	mov	bl, 110d
+	call 	linea_v
 	mov	si, 120d ; X -> Columna
 	mov 	di, 70d ; Y -> Fila
-	call 	linea7v
+	mov	bl, 90d
+	call 	linea_v
 	mov	si, 170d ; X -> Columna
 	mov 	di, 50d ; Y -> Fila
-	call 	linea9v
+	mov	bl, 70d
+	call 	linea_v
 
 	;HORIZONTALES
 	mov 	si, 100d ; X -> Columna
 	mov 	di, 150d ; Y -> Fila
-	call 	linea2h
+	mov	bl, 120d
+	call 	linea_h
 	mov	si, 120d ; X -> Columna
 	mov 	di, 110d ; Y -> Fila
-	call 	linea4h
+	mov	bl, 150d
+	call 	linea_h
 	mov	si, 120d ; X -> Columna
 	mov 	di, 90d ; Y -> Fila
-	call 	linea6h
-
+	mov	bl, 150d
+	call 	linea_h
 	mov	si, 120d ; X -> Columna
 	mov 	di, 70d ; Y -> Fila
-	call 	linea8h
-
+	mov	bl, 170d
+	call 	linea_h
 	mov	si, 100d ; X -> Columna
 	mov 	di, 50d ; Y -> Fila
-	call 	linea10h
+	mov	bl, 170d
+	call 	linea_h
 	call 	kb		; Utilizamos espera de alguna tecla
 
 	int 	20h
@@ -58,95 +66,23 @@ pixel:	mov	ah, 0Ch
 kb: 	mov	ah, 00h
 	int 	16h
 	ret
-linea1v: 
-lupi1:	mov 	cx, si ; Columna 
+linea_v: 
+lupi_v:	mov 	cx, si ; Columna 
 	mov	dx, 0d ; Fila
 	add 	dx, di
 	call 	pixel
 	inc 	di
-	cmp 	di, 150d
-	jne 	lupi1
+	cmp 	di, bx
+	jne 	lupi_v
 	ret
-linea2h:
-lupi2:	mov 	cx, 0d ; Columna 
+linea_h:
+lupi_h:	mov 	cx, 0d ; Columna 
 	add 	cx, si
 	mov	dx, di ; Fila
 	call 	pixel
 	inc 	si
-	cmp 	si, 120d
-	jne 	lupi2
+	cmp 	si, bx
+	jne 	lupi_h
 	ret
 
-linea3v: 
-lupi3:	mov 	cx, si ; Columna 
-	mov	dx, 0d ; Fila
-	add 	dx, di
-	call 	pixel
-	inc 	di
-	cmp 	di, 150d
-	jne 	lupi3
-	ret
-linea4h:
-lupi4:	mov 	cx, 0d ; Columna 
-	add 	cx, si
-	mov	dx, di ; Fila
-	call 	pixel
-	inc 	si
-	cmp 	si, 150d
-	jne 	lupi4
-	ret
-linea5v: 
-lupi5:	mov 	cx, si ; Columna 
-	mov	dx, 0d ; Fila
-	add 	dx, di
-	call 	pixel
-	inc 	di
-	cmp 	di, 110d
-	jne 	lupi5
-	ret
-linea6h:
-lupi6:	mov 	cx, 0d ; Columna 
-	add 	cx, si
-	mov	dx, di ; Fila
-	call 	pixel
-	inc 	si
-	cmp 	si, 150d
-	jne 	lupi6
-	ret
-linea7v: 
-lupi7:	mov 	cx, si ; Columna 
-	mov	dx, 0d ; Fila
-	add 	dx, di
-	call 	pixel
-	inc 	di
-	cmp 	di, 90d
-	jne 	lupi7
-	ret
-linea8h:
-lupi8:	mov 	cx, 0d ; Columna 
-	add 	cx, si
-	mov	dx, di ; Fila
-	call 	pixel
-	inc 	si
-	cmp 	si, 170d
-	jne 	lupi8
-	ret
-linea9v: 
-lupi9:	mov 	cx, si ; Columna 
-	mov	dx, 0d ; Fila
-	add 	dx, di
-	call 	pixel
-	inc 	di
-	cmp 	di, 70d
-	jne 	lupi9
-	ret
-linea10h:
-lupi10:	mov 	cx, 0d ; Columna 
-	add 	cx, si
-	mov	dx, di ; Fila
-	call 	pixel
-	inc 	si
-	cmp 	si, 170d
-	jne 	lupi10
-	ret
 section .data
